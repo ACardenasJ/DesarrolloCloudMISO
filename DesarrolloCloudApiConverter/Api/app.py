2ff83408ef150ebdf335d918f2b7c440a5c647ec
Andres Cardenas <a.cardenasj@uniandes.edu.co>
2022-10-15 13:20:22 -0500

    Actualizar para que inicien corriendo

--- DesarrolloCloudApiConverter/Api/app.py
+++ DesarrolloCloudApiConverter/Api/app.py
@@ -1,7 +1,9 @@
-from flaskr_temp import create_app
+from Api import create_app
 from markupsafe import escape
 from flask_restful import Api
-from flaskr_temp.vistas.vistas import VistaAPIGateway
+from Api.vistas.vistas import VistaAPIGateway
+
+
 
 app = create_app('default')
 app_context = app.app_context()
@@ -9,4 +11,5 @@ app_context.push()
 
 api = Api(app)
 
-api.add_resource(VistaAPIGateway, '/api')
+
+api.add_resource(VistaAPIGateway, '/registrar')
--- DesarrolloCloudApiConverter/Api/vistas/vistas.py
+++ DesarrolloCloudApiConverter/Api/vistas/vistas.py
@@ -7,14 +7,15 @@ import json
 
 
 class VistaAPIGateway(Resource):
-    def get(self):
+    def post(self):
         try:
-            url = 'http://localhost:5000/validarPass'
-            data = {'usuario' : request.json('usuario'),
-                    'contrasena': request.json('contrasena')}
+            url = 'http://localhost:5002/validarPass'
+            
+            data = {'usuario' : request.json['usuario'],
+                    'contrasena': request.json['contrasena']}                   
 
-            content = json.loads(requests.post(url,
-                                              data).get_data())
+            content = requests.post(url,json=data)
+            print(content.json())
             if content.status_code == 404:
                 return {'error': 'Servicio que valida la complejidad del passs no esta disponible'}, 404
             else:
@@ -24,14 +25,12 @@ class VistaAPIGateway(Resource):
                     return {'error': 'pass Invalido'}, 405
                 
                 url_back = 'http://localhost:5000/signin/user'
-                dataBudy = {'usuario' : request.json('contrasena'),
-                            'contrasena': request.json('contrasena'),
-                            'email': request.json["u_email"], 
+                dataBudy = {'usuario' : request.json['usuario'],
+                            'contrasena': request.json['contrasena'],
+                            'u_email': request.json["u_email"], 
                             'phone' : request.json["phone"]}
-                register = json.loads(requests.post(url_back,
-                                                        dataBudy).get_data())  
-                return json.dumps(register), 200
-              
+                register = requests.post(url_back,json=dataBudy)  
+                return register.json(), 200
         except ConnectionError as e:
             return {'error': 'Servicio InfoTemp offline -- Connection'}, 404
         except requests.exceptions.Timeout:
@@ -45,3 +44,10 @@ class VistaAPIGateway(Resource):
             return {'error': 'Servicio InfoTemp offline -- Request'}, 404
         except Exception as e:
             return {'error': 'Servicio InfoTemp - Error desconocido -' + str(e)}, 404
+    """Codigo para que juan trabaje y deje de mamar gallo
+    Codigo para que juan trabaje y deje de mamar gallo
+    Codigo para que juan trabaje y deje de mamar gallo
+    Codigo para que juan trabaje y deje de mamar gallo
+    Codigo para que juan trabaje y deje de mamar gallo
+    Codigo para que juan trabaje y deje de mamar gallo
+    Codigo para que juan trabaje y deje de mamar gallo"""
