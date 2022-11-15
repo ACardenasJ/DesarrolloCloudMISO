58b217c557bb56d55d00d834875064ea7fbf04bc
JUAN SEBASTIAN SANCHEZ L <js.sanchezl12@uniandes.edu.co>
2022-11-14 19:40:41 -0500

    funciones faltantes

--- DesarrolloCloudApiConverter/Converter_c/vistas/convertidor.py
+++ DesarrolloCloudApiConverter/Converter_c/vistas/convertidor.py
@@ -54,43 +54,57 @@ class converter():
         #guardar archivo en bucket
     #MP3 to WMA
     def mp3_WMA(self,file_name):
+        descargador(file_name+".mp3") 
         bashCommand = "ffmpeg -i "+UPLOAD_DIRECTORY+file_name+".mp3 "+PROCESS_DIRECTORY+file_name+".wma"
         process = subprocess.Popen(bashCommand.split(), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output, error = process.communicate()
         print(output,flush=True)
         print(error,flush=True)
+        subir_archivo(file_name+".wma")
     #WAV to MP3
     def wav_mp3(self,file_name):
+        descargador(file_name+".wav")
         bashCommand = "ffmpeg -i "+UPLOAD_DIRECTORY+file_name+".wav -vn -ar 44100 -ac 2 -b:a 192k "+PROCESS_DIRECTORY+file_name+".mp3"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
+        subir_archivo(file_name+".mp3")
 
     #WAV to WMA
     def wav_wma(self,file_name):
+        descargador(file_name+".wav")
         bashCommand = "ffmpeg -i "+UPLOAD_DIRECTORY+file_name+".wav "+PROCESS_DIRECTORY+file_name+".wma"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
+        subir_archivo(file_name+".wma")
 
     #WMA to MP3
     def wma_mp3(self,file_name):
+        descargador(file_name+".wma")
         bashCommand = "ffmpeg -i "+UPLOAD_DIRECTORY+file_name+".wma -f mp3 -ab 192 "+PROCESS_DIRECTORY+file_name+".mp3"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
+        subir_archivo(file_name+".mp3")
 
     #WMA to WAV
     def WMA_WAV(self,file_name):
+        descargador(file_name+".wma")
         bashCommand = "ffmpeg -i "+UPLOAD_DIRECTORY+file_name+".wma "+PROCESS_DIRECTORY+file_name+".wav"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
+        subir_archivo(file_name+".wav")
 
     #MP3 to AAC
     def mp3_acc(self,file_name):
+        descargador(file_name+".mp3")
         bashCommand = "ffmpeg  -i "+UPLOAD_DIRECTORY+file_name+".mp3 -c:a aac -vn "+PROCESS_DIRECTORY+file_name+".m4a"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
+        descargador(file_name+".m4a")
 
     #MP3 to OGG
     def mp3_ogg(self,file_name):
+        descargador(file_name+".mp3")
         bashCommand = " ffmpeg -y -i "+UPLOAD_DIRECTORY+file_name+".mp3  -strict -2 -acodec vorbis -ac 2 -aq 50 "+PROCESS_DIRECTORY+file_name+".ogg"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
+        descargador(file_name+".ogg")
