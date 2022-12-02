9e2b303a802cdd005edf568dee5d425b5fbb145b
JUAN SEBASTIAN SANCHEZ L <js.sanchezl12@uniandes.edu.co>
2022-12-02 00:06:16 -0500

    bucket implementacion cola

--- DesarrolloCloudApiConverter/Bucket/vistas/vistas.py
+++ DesarrolloCloudApiConverter/Bucket/vistas/vistas.py
@@ -8,12 +8,18 @@ from google.oauth2 import service_account
 import json
 import requests
 from urllib.request import urlopen
+import queue
+import threading
 import time
-
 import random
+
 url = "https://storage.googleapis.com/bucket_music_file_storage_1/enviroments.json?rand_v="+str(random.randint(1,10000000))
 response = urlopen(url)
 data = json.loads(response.read())
+
+# cola_upfiles = queue.Queue()
+# cola_pofiles = queue.Queue()
+cola_bucketfiles = queue.Queue()
 
 BACKEND_URI = data['BACKEND_URL']
 print(url,flush=True)
@@ -40,14 +46,8 @@ class MBucketPOST(Resource):
             file = request.files['file']
             file_path = os.path.join(UPLOAD_DIRECTORY, file_name)
             file.save(file_path)
-            bucket = storage_client.get_bucket(bucket_name)
-            blob = bucket.blob("upfiles/"+file_name)
-            blob.upload_from_filename(file_path)
-            print('FILE UPLOADED')
-            url_back = 'http://{}/api/taskUpdSt/{}'.format(BACKEND_URI,id_task)
-            actualizar = requests.put(url_back, json={"status":"En Proceso"})
-            print('TASK UPDATED')
-            time.sleep(3)
+            file_data = {'file_name': file_name, 'id_task': id_task, 'file_path': file_path, 'bucket': 'UP'}
+            cola_bucketfiles.put(file_data)
             return {'status': 'ok'}, 200
 
 class ManageBucketUP(Resource):
@@ -72,13 +72,8 @@ class ManageBucketPOST2_(Resource):
         file = request.files['file']
         file_path = os.path.join(PROCESS_DIRECTORY, file_name)
         file.save(file_path)
-        bucket = storage_client.get_bucket(bucket_name)
-        blob = bucket.blob("pofiles/"+file_name)
-        blob.upload_from_filename(file_path)
-        print('FILE UPLOADED')
-        url_back = 'http://{}/api/taskUpd/{}'.format(BACKEND_URI,id_task)
-        actualizar = requests.put(url_back)
-        time.sleep(3)
+        file_data = {'file_name': file_name, 'id_task': id_task, 'file_path': file_path, 'bucket': 'PO'}
+        cola_bucketfiles.put(file_data)
         return {'status': 'ok'}, 200
 
 
@@ -98,3 +93,86 @@ class ManageBucketPO(Resource):
             else:
                 return {'status': 'error'}, 404
 
+
+# # Funcion para procesar la cola
+# def func_upfiles(q, thread_no):
+#     while True:
+#         file_data = q.get()
+#         file_name = file_data['file_name']
+#         id_task = file_data['id_task']
+#         file_path = file_data['file_path']
+
+#         bucket = storage_client.get_bucket(bucket_name)
+#         blob = bucket.blob("upfiles/"+file_name)
+#         blob.upload_from_filename(file_path)
+#         #print('UP - FILE UPLOADED',flush=True)
+#         url_back = 'http://{}/api/taskUpdSt/{}'.format(BACKEND_URI,id_task)
+#         actualizar = requests.put(url_back, json={"status":"En Proceso"})
+#         #print('UP - TASK UPDATED',flush=True)
+#         time.sleep(3)
+#         q.task_done()
+#         print(f'Thread #{thread_no} processed task #{id_task} in the queue. UPFILE - {file_name}',flush=True)
+
+
+#         # task = q.get()
+#         # time.sleep(2)
+#         # q.task_done()
+#         # print(f'Thread #{thread_no} is doing task #{task} in the queue.')
+
+
+# Funcion para procesar la cola
+# def func_pofiles(q, thread_no):
+#     while True:
+#         file_data = q.get()
+#         file_name = file_data['file_name']
+#         id_task = file_data['id_task']
+#         file_path = file_data['file_path']
+
+#         bucket = storage_client.get_bucket(bucket_name)
+#         blob = bucket.blob("pofiles/"+file_name)
+#         blob.upload_from_filename(file_path)
+#         #print('PO - FILE UPLOADED',flush=True)
+#         url_back = 'http://{}/api/taskUpd/{}'.format(BACKEND_URI,id_task)
+#         actualizar = requests.put(url_back)
+#         time.sleep(3)
+#         q.task_done()
+#         print(f'Thread #{thread_no} processed task #{id_task} in the queue. POFILE - {file_name}',flush=True)
+
+def fun_bucket_files(q, thread_no):
+    while True:
+        file_data = q.get()
+        bucket = file_data['bucket']
+        file_name = file_data['file_name']
+        id_task = file_data['id_task']
+        file_path = file_data['file_path']
+        if (bucket == 'UP'):
+            bucket = storage_client.get_bucket(bucket_name)
+            blob = bucket.blob("upfiles/"+file_name)
+            blob.upload_from_filename(file_path)
+            #print('UP - FILE UPLOADED',flush=True)
+            url_back = 'http://{}/api/taskUpdSt/{}'.format(BACKEND_URI,id_task)
+            actualizar = requests.put(url_back, json={"status":"En Proceso"})
+            #print('UP - TASK UPDATED',flush=True)
+            time.sleep(3)
+            q.task_done()
+            print(f'Thread processed task #{id_task} in the queue. UPFILE - {file_name}',flush=True)
+        elif (bucket == 'PO'):         
+            bucket = storage_client.get_bucket(bucket_name)
+            blob = bucket.blob("pofiles/"+file_name)
+            blob.upload_from_filename(file_path)
+            #print('PO - FILE UPLOADED',flush=True)
+            url_back = 'http://{}/api/taskUpd/{}'.format(BACKEND_URI,id_task)
+            actualizar = requests.put(url_back)
+            time.sleep(3)
+            q.task_done()
+            print(f'Thread processed task #{id_task} in the queue. POFILE - {file_name}',flush=True)
+
+
+# Crea 1 worker thread
+for i in range(1):
+    # worker_upfiles = threading.Thread(target=func_upfiles, args=(cola_upfiles, i,), daemon=True)
+    # worker_pofiles = threading.Thread(target=func_pofiles, args=(cola_pofiles, i,), daemon=True)
+    # worker_upfiles.start()
+    # worker_pofiles.start()
+    worker_files_bucket = threading.Thread(target=fun_bucket_files, args=(cola_bucketfiles, i,), daemon=True)
+    worker_files_bucket.start()
